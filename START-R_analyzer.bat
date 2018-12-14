9f0b7cb40e3d6b5e6e37b3eb28f73f7e2938626f
thomasdenecker <thomas.denecker@gmail.com>
2018-12-14 12:56:56 +0100

    clear

--- START-R_analyzer.bat
+++
@@ -1,2 +0,0 @@
-docker run --rm -p 3838:3838 -v C:\Users\thomas\Desktop\START-R\START-R_analyzer:/srv/shiny-server tdenecker/start-r 
-docker run --rm -p 3838:3838 -v C:\Users\thomas\Desktop\START-R\START-R_analyzer:/srv/shiny-server tdenecker/start-r 
