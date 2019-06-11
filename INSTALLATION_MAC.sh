885218479f2449f6a4aff4630f4e53cbd823e2ef
thomasdenecker <thomas.denecker@gmail.com>
2019-06-11 16:50:52 +0200

    Update INSTALLATION_MAC.sh

--- INSTALLATION_MAC.sh
+++ INSTALLATION_MAC.sh
@@ -13,3 +13,4 @@ echo 'docker run --rm -p 3839:3838 -v' $BASEDIR'/START-R_viewer:/srv/shiny-serve
 
 chmod +x $BASEDIR/START-R_analyzer.sh
 chmod +x $BASEDIR/START-R_viewer.sh
+mkdir $BASEDIR/START-R_analyzer/Outputs
